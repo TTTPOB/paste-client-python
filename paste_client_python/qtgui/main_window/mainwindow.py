d2024b8c5f7227d343356f33ae2851d0c92e21a3
tpob <tpob@tpob.xyz>
2022-01-10 21:33:02 +0800

    remove unused imports

--- paste_client_python/qtgui/main_window/mainwindow.py
+++ paste_client_python/qtgui/main_window/mainwindow.py
@@ -1,9 +1,5 @@
 from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtGui import QGuiApplication
 # from system_tray import system_tray
-import random
-from PySide6.QtWebSockets import QWebSocket
-from ..system_tray import system_tray
 from .window import Ui_main_window
 
 
