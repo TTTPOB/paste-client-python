4945def5fa01166d43494c3d699b982618187251
tpob <tpob@tpob.xyz>
2022-01-10 19:43:42 +0800

    format code

--- paste_client_python/qtgui/config_page/config_page.py
+++ paste_client_python/qtgui/config_page/config_page.py
@@ -12,12 +12,10 @@ class config_page(QWidget):
         self.ui = Ui_config_page()
         self.ui.setupUi(self)
         self.ui.generate_new_keypair_botton.clicked.connect(self.generate_new_keypair)
-    
+
     def generate_new_keypair(self):
         key_pair = PrivateKey.generate()
-        self.ui.pubkey_content.setText(key_pair.public_key.encode(encoder=HexEncoder).decode())
+        self.ui.pubkey_content.setText(
+            key_pair.public_key.encode(encoder=HexEncoder).decode()
+        )
         self.ui.privkey_content.setText(key_pair.encode(encoder=HexEncoder).decode())
-
-        
-    
-    
